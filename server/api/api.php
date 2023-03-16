f640afa8e83191906923838342ae9fe64fdfe275
Duy Nguyen <2051067158@e.tlu.edu.vn>
2023-03-17 00:37:33 +0700

    module thao tác REST API

--- server/api/api.php
+++ server/api/api.php
@@ -3,19 +3,28 @@ require __DIR__."/../inc/bootstrap.php";
 
 const ERROR_RESPONSE = 404;
 const SUCCESS_RESPONSE = 200;
-function api_error_response($message){
-    api_msg_response($message, ERROR_RESPONSE);
+function api_error_response($message, bool $status = null){
+    api_msg_response($message, ERROR_RESPONSE, $status);
 }
 
-function api_success_response($message){
-    api_msg_response($message, SUCCESS_RESPONSE);
+function api_success_response($message, bool $status = null){
+    api_msg_response($message, SUCCESS_RESPONSE, $status);
 }
 
-function api_msg_response($message, int $statusCode){
+function api_msg_response($message, int $statusCode, bool $status = null){
     $response['message'] = $message;
+    if(!is_null($status)){
+        $response['status'] = $status;
+    }
     api_response($response, $statusCode);
-
 }
+
+
+/**
+ * @param array $response
+ * @param int $statusCode
+ * @return void
+ */
 function api_response(array $response, int $statusCode){
     header('Content-Type: application/json; charset=utf-8');
     http_response_code($statusCode);
@@ -31,7 +40,11 @@ function api_checking_connection($connection){
 
 function api_query($query){
     global $connection;
-    return mysqli_query($connection, $query);
+    try {
+        return mysqli_query($connection, $query);
+    }catch (mysqli_sql_exception $exception){
+        return null;
+    }
 }
 
 $connection = getConnection();
