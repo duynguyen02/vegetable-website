652c54e5a61a735aa908d28476653859c6a39f26
Duy Nguyen <2051067158@e.tlu.edu.vn>
2023-03-17 00:39:22 +0700

    chỉnh tệp cấu hình server

---
+++ server/inc/config.example.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Đây là tệp chứa các mẫu cấu hình của server
+ * Tạo một tệp config.php đồng cấp với tệp này
+ * Và sửa các giá trị cấu hình để phù hợp
+ * với máy chủ đang sử dụng.
+ *
+ * Đối số bao gồm:
+ * DB_HOST: máy chủ của cơ sở dữ liệu
+ * DB_USERNAME: tên người dùng cơ sở dữ liệu
+ * DB_PASSWORD: mật khẩu cơ sở dữ liệu
+ * DB_DATABASE_NAME: tên cơ sở dữ liệu
+ */
+const DB_HOST = "";
+const DB_USERNAME = "";
+const DB_PASSWORD = "";
+const DB_DATABASE_NAME = "";
+
--- server/inc/config.php
+++ server/inc/config.php
@@ -1,6 +1,6 @@
 <?php
-const DB_HOST = "localhost";
-const DB_USERNAME = "root";
+const DB_HOST = "";
+const DB_USERNAME = "";
 const DB_PASSWORD = "";
-const DB_DATABASE_NAME = "vegetable_showroom";
+const DB_DATABASE_NAME = "";
 
