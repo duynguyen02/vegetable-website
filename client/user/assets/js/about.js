1e4250dde61d52885d6469026f44341c3aa7f530
Duy Nguyen <2051067158@e.tlu.edu.vn>
2023-03-20 17:08:03 +0700

    chỉnh sửa giao diện người dùng lần 1

---
+++ client/user/assets/js/about.js
@@ -0,0 +1,6 @@
+(function(){
+    "use strict";
+
+    // khởi tạo bộ đếm 
+    new PureCounter();
+})()
---
+++ client/user/assets/js/main-page.js
@@ -0,0 +1,54 @@
+(function () {
+  "use strict";
+
+  // khởi tạo card sản phẩm tiêu biểu
+
+  const portfolioLightbox = GLightbox({
+    selector: ".portfolio-lightbox",
+  });
+
+
+  new Swiper(".portfolio-details-slider", {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+  });
+
+  // khởi tạo card đánh giá của khách hàng
+  new Swiper(".testimonials-slider", {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: "auto",
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+    },
+  });
+
+
+
+})();
